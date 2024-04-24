9c807529d9cae392c05edd3940a94cebc2d1075d
pablohigu <pablo.hi.do@gmail.com>
2024-04-24 09:27:42 +0200

    Metas

--- Entidades/Metas.vb
+++ Entidades/Metas.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Metas
-
+    Public Property NumODS As Integer
+    Public Property CodMeta As String
+    Public Property Nombre As String
+    Public Property Descripcion As String
 End Class
