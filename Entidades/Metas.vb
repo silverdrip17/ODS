749ec1117c070bdfcb71a24344fff39a2986d9f7
pablohigu <pablo.hi.do@gmail.com>
2024-04-24 09:36:57 +0200

    Equals Metas

--- Entidades/Metas.vb
+++ Entidades/Metas.vb
@@ -1,6 +1,18 @@
 ﻿Public Class Metas
+    Implements IEquatable(Of Metas)
+
     Public Property NumODS As Integer
     Public Property CodMeta As String
     Public Property Nombre As String
     Public Property Descripcion As String
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return Equals(TryCast(obj, Metas))
+    End Function
+
+    Public Overloads Function Equals(other As Metas) As Boolean Implements IEquatable(Of Metas).Equals
+        Return other IsNot Nothing AndAlso
+               NumODS = other.NumODS AndAlso
+               CodMeta = other.CodMeta
+    End Function
 End Class
