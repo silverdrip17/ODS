5ddc45fc96cf55b6adefc35573d62a74518dc445
Danel Rico <danelricofer@gmail.com>
2024-04-24 09:30:26 +0200

    Cambio en propiedad NumODS de ODS

--- Entidades/ODS.vb
+++ Entidades/ODS.vb
@@ -1,6 +1,5 @@
 ﻿Public Class ODS
-    Public Property NumeroOds As Byte
-
+    Public Property NumODS As Byte
     Public Property Nombre As String
     Public Property Descripcion As String
 End Class
