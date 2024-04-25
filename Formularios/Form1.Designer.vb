7194a4bd58d12e78f7aee187c8ba018e74b03bfc
silverdrip17 <kevinlopes566@gmail.com>
2024-04-25 11:59:54 +0200

    diseño form

--- Formularios/Form1.Designer.vb
+++ Formularios/Form1.Designer.vb
@@ -23,33 +23,84 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.grpEditar = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grpEditar.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(425, 109)
+        Me.DataGridView1.Location = New System.Drawing.Point(760, 2)
         Me.DataGridView1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.RowHeadersWidth = 62
         Me.DataGridView1.RowTemplate.Height = 28
-        Me.DataGridView1.Size = New System.Drawing.Size(625, 197)
+        Me.DataGridView1.Size = New System.Drawing.Size(846, 324)
         Me.DataGridView1.TabIndex = 0
+        '
+        'grpEditar
+        '
+        Me.grpEditar.Controls.Add(Me.Label3)
+        Me.grpEditar.Controls.Add(Me.Label2)
+        Me.grpEditar.Controls.Add(Me.Label1)
+        Me.grpEditar.Location = New System.Drawing.Point(759, 354)
+        Me.grpEditar.Name = "grpEditar"
+        Me.grpEditar.Size = New System.Drawing.Size(832, 308)
+        Me.grpEditar.TabIndex = 1
+        Me.grpEditar.TabStop = False
+        Me.grpEditar.Text = "Editar ods y/o Metas"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ODS:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Editar ODS:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(49, 186)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(103, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Editar Metas:"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1438, 542)
+        Me.ClientSize = New System.Drawing.Size(1618, 678)
+        Me.Controls.Add(Me.grpEditar)
         Me.Controls.Add(Me.DataGridView1)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grpEditar.ResumeLayout(False)
+        Me.grpEditar.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents grpEditar As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
