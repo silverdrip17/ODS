db19965e80b6c093c45b7e123007cbbd47823253
Edu <efontsefer@educacion.navarra.es>
2024-04-25 13:26:34 +0200

    Añadida cadena de conexion

--- Gestion/GestionIniciativas.vb
+++ Gestion/GestionIniciativas.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Collections.ObjectModel
 Imports Entidades
 Public Class GestionIniciativas
+    Private cadenaDeConexion As String = "Data Source = *; Initial Catalog = Estado; Integrated Security = SSPI; MultipleActiveResultSets=true"
     Public ReadOnly Property MisODS As ReadOnlyCollection(Of ODS)
 
     Public Sub New()
