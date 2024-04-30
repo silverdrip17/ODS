dcbb5d9e65223a0313aa6e76ca7374c7906cf69f
silverdrip17 <kevinlopes566@gmail.com>
2024-04-30 13:05:01 +0200

    EMPEZANDO a leer de la base de datos ods

--- Gestion/GestionIniciativas.vb
+++ Gestion/GestionIniciativas.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Collections.ObjectModel
 Imports Entidades
 Public Class GestionIniciativas
-    Private cadenaDeConexion As String = "Data Source = *; Initial Catalog = Estado; Integrated Security = SSPI; MultipleActiveResultSets=true"
+    Private cadenaDeConexion As String = "Data Source = *; Initial Catalog = PROYECTOODS; Integrated Security = SSPI; MultipleActiveResultSets=true"
     Public ReadOnly Property MisODS As ReadOnlyCollection(Of ODS)
 
     Public Sub New()
@@ -10,6 +10,10 @@ Public Class GestionIniciativas
 
     Public Function devolverODS()
         Return MisODS
+    End Function
+
+    Public Function guardarOds(ByRef msg As String) As ReadOnlyCollection(Of ODS)
+
     End Function
 
 End Class
