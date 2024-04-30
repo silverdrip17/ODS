a2384ab2e6b40fc628272ddb986b6d80572af3de
pablohigu <pablo.hi.do@gmail.com>
2024-04-30 14:24:32 +0200

    Formulario Cambiar Metas

--- Formularios/CambioMetas.vb
+++ Formularios/CambioMetas.vb
@@ -1,3 +1,9 @@
 ﻿Public Class CambioMetas
+    Private Sub CambioMetas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Descripcion_Click(sender As Object, e As EventArgs) Handles lblDescripcion.Click
+
+    End Sub
 End Class
--- Formularios/Form1.vb
+++ Formularios/Form1.vb
@@ -8,13 +8,6 @@ Public Class Form1
     End Sub
 
     Private Sub btnTodasOds_Click_1(sender As Object, e As EventArgs) Handles btnTodasOds.Click
-        Dim msg As String = ""
-        Dim listaOds As ReadOnlyCollection(Of ODS)
-
-        dgvOds.DataSource = False
-
-        listaOds = Gestor.GuardarOds(msg)
-        dgvOds.DataSource = listaOds
 
     End Sub
 End Class
