56507bca396276ae5a959ad8166fbcd8264af736
silverdrip17 <kevinlopes566@gmail.com>
2024-04-30 13:38:34 +0200

    conseguir ods de la base de datos completado

--- Entidades/ODS.vb
+++ Entidades/ODS.vb
@@ -16,12 +16,18 @@ Public Class ODS
         Me.NumODS = numODS
     End Sub
 
-    Public Sub New(numODS As Integer, nombre As String, descripcion As String, imagen As String)
+    Public Sub New(numODS As Integer, nombre As String, descripcion As String)
         Me.New(numODS)
         Me.Nombre = nombre
         Me.Descripcion = descripcion
-        Me.Imagen = imagen
     End Sub
+
+    'Public Sub New(numODS As Integer, nombre As String, descripcion As String, imagen As String)
+    '    Me.New(numODS)
+    '    Me.Nombre = nombre
+    '    Me.Descripcion = descripcion
+    '    Me.Imagen = imagen
+    'End Sub
 
     Public Overrides Function Equals(obj As Object) As Boolean
         Return Equals(TryCast(obj, ODS))
--- Gestion/GestionIniciativas.vb
+++ Gestion/GestionIniciativas.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Collections.ObjectModel
+Imports System.Data.SqlClient
 Imports Entidades
 Public Class GestionIniciativas
     Private cadenaDeConexion As String = "Data Source = *; Initial Catalog = PROYECTOODS; Integrated Security = SSPI; MultipleActiveResultSets=true"
@@ -8,15 +9,33 @@ Public Class GestionIniciativas
 
     End Sub
 
-    Public Function devolverODS()
+    Public Function DevolverODS()
         Return MisODS
     End Function
 
-    Public Function guardarOds(ByRef msg As String) As ReadOnlyCollection(Of ODS)
-
+    Public Function GuardarOds(ByRef msg As String) As ReadOnlyCollection(Of ODS)
+        Dim todosLosOds As New List(Of ODS)
+        msg = ""
+        Dim oConexion As New SqlConnection(cadenaDeConexion)
+        Try
+            oConexion.Open()
+            Dim sql As String = "select NUMERO, NOMBRE, DESCRIPCIO FROM PROYECTOODS"
+            Dim cmdLeer As New SqlCommand(sql, oConexion)
+            Dim dr As SqlDataReader = cmdLeer.ExecuteReader
+            Do While dr.Read
+                Dim odss As New ODS(dr("NUMERO").ToString, dr("NOMBRE").ToString, dr("DESCRIPCION").ToString)
+                todosLosOds.Add(odss)
+            Loop
+        Catch ex As Exception
+            msg = ex.Message
+            Return Nothing
+        Finally
+            oConexion.Close()
+        End Try
+        Return todosLosOds.AsReadOnly
     End Function
 
-    Public Function devolverMeta(numODS As Integer)
+    Public Function DevolverMeta(numODS As Integer)
         If numODS > 17 Then
             Return Nothing
         End If
