40b934bbccaa8d79ef8180f45e80b8f957641e2b
Danel Rico <danelricofer@gmail.com>
2024-05-02 12:45:31 +0200

    Cambios en el código del formulario de las METAS

--- Formularios/CambioMetas.vb
+++ Formularios/CambioMetas.vb
@@ -1,4 +1,5 @@
-﻿Imports Gestion
+﻿Imports Entidades
+Imports Gestion
 
 Public Class CambioMetas
     Private Sub CambioMetas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -10,6 +11,12 @@ Public Class CambioMetas
 
 
     Private Sub cboods_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboods.SelectedIndexChanged
-
+        Dim odsSeleccionado As ODS = TryCast(cboods.SelectedItem, ODS)
+        If odsSeleccionado Is Nothing Then
+            Exit Sub
+        End If
+        For i As Integer = 0 To odsSeleccionado.ListaMetas.Count - 1
+            cboMetas.Items.Add(odsSeleccionado.ListaMetas(i))
+        Next
     End Sub
 End Class
