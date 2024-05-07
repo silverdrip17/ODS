60685f00dd8371fde996ff5d6e851ffe33bc30be
Edu <efontsefer@educacion.navarra.es>
2024-05-07 14:22:08 +0200

    Añadido constructor Iniciativa

--- Entidades/Iniciativa.vb
+++ Entidades/Iniciativa.vb
@@ -1,8 +1,33 @@
 ﻿Public Class Iniciativa
+    Implements IEquatable(Of Iniciativa)
+
     Public Property CodIniciativa As Integer
     Public Property Titulo As String
     Public Property Descripcion As String
     Public Property FechaInicio As Date
     Public Property FechaFin As Date
 
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(codIniciativa As Integer)
+        Me.CodIniciativa = codIniciativa
+    End Sub
+    Public Sub New(codIniciativa As Integer, titulo As String, descripcion As String, fechaInicio As Date, fechaFin As Date)
+        Me.New(codIniciativa)
+        Me.Titulo = titulo
+        Me.Descripcion = descripcion
+        Me.FechaInicio = fechaInicio
+        Me.FechaFin = fechaFin
+    End Sub
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return Equals(TryCast(obj, Iniciativa))
+    End Function
+
+    Public Overloads Function Equals(other As Iniciativa) As Boolean Implements IEquatable(Of Iniciativa).Equals
+        Return other IsNot Nothing AndAlso
+               CodIniciativa = other.CodIniciativa
+    End Function
 End Class
