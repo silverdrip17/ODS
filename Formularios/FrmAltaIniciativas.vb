af46c362bef4af46cb488e3d6c191e16bedd7478
silverdrip17 <kevinlopes566@gmail.com>
2024-05-08 09:40:55 +0200

    correcciones

--- Formularios/FrmAltaIniciativas.vb
+++ Formularios/FrmAltaIniciativas.vb
@@ -2,7 +2,7 @@
 
 Public Class FrmAltaIniciativas
     Private Sub FrmAltaIniciativas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cboOds.Items.AddRange(Gestor.MisODS.ToArray)
+        'cboOds.Items.AddRange(Gestor.MisODS.ToArray)
         lstMetas.Hide()
     End Sub
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOds.SelectedIndexChanged
