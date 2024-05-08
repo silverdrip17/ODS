e004e81bfdf04a894ea80bca129c1fdee52c5e62
Edu <efontsefer@educacion.navarra.es>
2024-05-08 09:27:12 +0200

    Guardar ODS en Iniciativas

--- Formularios/FrmAltaIniciativas.vb
+++ Formularios/FrmAltaIniciativas.vb
@@ -2,7 +2,8 @@
 
 Public Class FrmAltaIniciativas
     Private Sub FrmAltaIniciativas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cboOds.Items.AddRange(Gestor.MisODS.ToArray)
+        Dim msg As String = ""
+        cboOds.Items.AddRange(Gestor.DevolverOds(msg).ToArray)
         lstMetas.Hide()
     End Sub
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOds.SelectedIndexChanged
