79d9a6f3cf8aee5f43b4b5d584e22042b7dda070
Edu <efontsefer@educacion.navarra.es>
2024-05-16 12:04:13 +0200

    Modificado constructor Clase Iniciativa

--- Entidades/Iniciativa.vb
+++ Entidades/Iniciativa.vb
@@ -15,21 +15,29 @@ Public Class Iniciativa
             Return _Solicitante
         End Get
     End Property
-    Public ReadOnly Property ListaMetas As ReadOnlyCollection(Of Metas)
-    Public ReadOnly Property Profesores As ReadOnlyCollection(Of Profesor)
-    Public ReadOnly Property Modulos As ReadOnlyCollection(Of Modulo)
+    Public Property ListaMetas As List(Of Metas)
+    Public Property Profesores As List(Of Profesor)
+    Public Property Modulos As List(Of Modulo)
     Public Sub New()
+        ListaMetas = New List(Of Metas)
+        Profesores = New List(Of Profesor)
+        Modulos = New List(Of Modulo)
     End Sub
 
-    Public Sub New(codIniciativa As Integer, titulo As String)
-        Me.CodIniciativa = codIniciativa
-        Me.Titulo = titulo
-    End Sub
+
     Public Sub New(codIniciativa As Integer, titulo As String, descripcion As String, fechaInicio As Date, fechaFin As Date)
-        Me.New(codIniciativa, titulo)
+        Me.New()
         Me.Descripcion = descripcion
         Me.FechaInicio = fechaInicio
         Me.FechaFin = fechaFin
+    End Sub
+
+    Public Sub New(codIniciativa As Integer, titulo As String, descripcion As String, fechaInicio As Date, fechaFin As Date, solicitante As Solicitante, listaMetas As List(Of Metas), profesores As List(Of Profesor), modulos As List(Of Modulo))
+        Me.New(codIniciativa, titulo, descripcion, fechaInicio, fechaFin)
+        _Solicitante = solicitante
+        Me.ListaMetas = listaMetas
+        Me.Profesores = profesores
+        Me.Modulos = modulos
     End Sub
 
 
